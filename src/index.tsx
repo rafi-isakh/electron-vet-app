752c964db96815f59c3276ceffeffe2f127ab2c6
rafi-isakh <rafiisakh@gmail.com>
2020-06-15 16:28:35 +0700

    setup initial dependency

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { createHashHistory } from 'history';
+import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import configureStore from './store';
+import Root from './containers/Root';
+
+const store = configureStore();
+const history = createHashHistory();
+
+const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+
+document.addEventListener('DOMContentLoaded', () =>
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createHashHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+import createRootReducer from '../reducers';
+import { Store, counterStateType } from '../reducers/types';
+
+const history = createHashHistory();
+const rootReducer = createRootReducer(history);
+const router = routerMiddleware(history);
+const enhancer = applyMiddleware(thunk, router);
+
+export default function configureStore(initialState?: counterStateType): Store {
+  return createStore(rootReducer, initialState, enhancer);
+}
